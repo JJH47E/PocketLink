10e6ac3149133526864f0f5aec6b05402b1de691
JJH <62762383+JJH47E@users.noreply.github.com>
2024-07-05 19:28:01 +0100

    Initial Commit

---
+++ Pocket/PocketApp.swift
@@ -0,0 +1,32 @@
+//
+//  PocketApp.swift
+//  Pocket
+//
+//  Created by JJ Hayter on 05/07/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PocketApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
