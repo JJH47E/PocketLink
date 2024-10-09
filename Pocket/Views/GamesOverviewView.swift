859ade2b3aa715cc8a74b808ecb30cd3510b9c0b
JJH <62762383+JJH47E@users.noreply.github.com>
2024-10-09 19:19:47 +0100

    add backup all saves button

--- Pocket/Views/GamesOverviewView.swift
+++ Pocket/Views/GamesOverviewView.swift
@@ -12,22 +12,59 @@ struct GamesOverviewView: View {
     @State private var selectedGame: Game.ID?
     
     var body: some View {
-        Table(of: Game.self, selection: $selectedGame) {
-            TableColumn("Platform", value: \.displayName)
-            TableColumn("Platform", value: \.platform)
-        } rows: {
-            ForEach(games) { game in
-                TableRow(game)
-                    .contextMenu {
-                        if (game.savePath != nil) {
-                            Button("Backup Save") {
-                                openSaveDialog(filePath: game.savePath!)
+        VStack {
+            Table(of: Game.self, selection: $selectedGame) {
+                TableColumn("Platform", value: \.displayName)
+                TableColumn("Platform", value: \.platform)
+            } rows: {
+                ForEach(games) { game in
+                    TableRow(game)
+                        .contextMenu {
+                            if (game.savePath != nil) {
+                                Button("Backup Save") {
+                                    openSaveDialog(filePath: game.savePath!)
+                                }
+                            }
+                            Button("Backup ROM") {
+                                openSaveDialog(filePath: game.path)
                             }
                         }
-                        Button("Backup ROM") {
-                            openSaveDialog(filePath: game.path)
-                        }
-                    }
+                }
+            }
+            HStack {
+                Spacer()
+                Button() {
+                    openBackupAllDialog()
+                } label: {
+                    Text("Backup All Saves")
+                }.disabled(backupAllButtonDisabled()).padding()
+            }
+        }
+    }
+    
+    func backupAllButtonDisabled() -> Bool {
+        for game in games {
+            if (game.savePath != nil) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func openBackupAllDialog() {
+        let panel = NSOpenPanel()
+        panel.title = "Select destination to copy saves"
+        panel.canCreateDirectories = true
+        panel.showsTagField = false
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+
+        if panel.runModal() == .OK, let selectedURL = panel.url {
+            for game in games {
+                if (game.savePath == nil) {
+                    continue
+                }
+                copyFile(source: game.savePath!, destination: selectedURL.appendingPathComponent(game.savePath!.lastPathComponent))
             }
         }
     }
